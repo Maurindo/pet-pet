2fd13cef81174e2af329c40f3ca98a6f95f1c94b
Maurindo Junior <maurindoapgjunior@gmail.com>
2024-01-11 08:37:50 -0300

    rota img footer raiz

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css"
 import logo from "./imagens/logo-1.png"
 
 function Footer() {
     return(
         <footer className={styles.footer}>
-            <img  src={logo} alt="imagem logo cão"/>
+           <Link to="/"><img  src={logo} alt="imagem logo cão"/></Link>
         </footer>
 
     );
