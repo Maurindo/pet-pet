d22a6e70ca3b99d038ccb52a951ae33f928bc121
Maurindo Junior <maurindoapgjunior@gmail.com>
2024-01-20 00:13:56 -0300

    page serviços carrocel teste

--- src/Components/Contatos/index.js
+++ src/Components/Contatos/index.js
@@ -1,11 +1,28 @@
 import styles from "./Contato.module.css"
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel  from 'react-bootstrap/Carousel';
 function ContatosPet(){
     return(
-        <div className={styles.contato}>
-            <h1>Contatos</h1>
-        </div>
-    );
+    <div>
+      <h4>React-Bootstrap Carousel Component</h4>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="Image One"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Image Two"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
-
+      
+ 
 export default ContatosPet;
--- src/Components/Servicos/index.js
+++ src/Components/Servicos/index.js
@@ -1,10 +1,39 @@
 import styles from "./Servicos.module.css"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel  from 'react-bootstrap/Carousel';
+import img1 from './imagens/dog_1.jpg'
+import img2 from './imagens/dog_2.jpeg'
+import img3 from './imagens/dog_3.jpeg'
 
 function ServicosPet(){
     return(
-        <div className={styles.servicos}>
-            <h1>Serviços pet-pet</h1>
-        </div>
+        <div className={styles.painel}>
+        <h4>Com maior amor para seus bicnhinhos</h4>
+        <Carousel>
+          <Carousel.Item interval={1250}>
+            <img
+              className={styles.imgDog}  
+              src={img1}
+              alt="Image One"
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={1250}>
+            <img
+              className={styles.imgDog}  
+              src={img2}
+              alt="Image Two"
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={1250}>
+            <img
+               className={styles.imgDog} 
+                src={img3}
+                alt="image 3"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     );
 }
 
