a095e019c226f08f9953bfeb794da827348acc70
MAURINDO JUNIOR <144647505+Maurindo@users.noreply.github.com>
2024-09-06 23:38:55 -0300

    Update index.js
    arrumando contato

--- src/Components/Contatos/index.js
+++ src/Components/Contatos/index.js
@@ -1,11 +1,28 @@
 import styles from "./Contato.module.css";
 import React from 'react';
+
+
+
 function ContatosPet(){
-    return(
-    <div>
-        <h1>Contato</h1>
-    </div>
-  );
+    return (
+      <div className={styles.corpo}>
+        <h4>Envia sua perguntas</h4>
+        <form>
+          <label>e-mail</label>
+          <br />
+          <input type="text" />
+          <br />
+          <label>Telefone</label>
+          <br />
+          <input type="text" />
+          <br />
+          <br />
+          <textarea></textarea>
+          <br />
+          <button>Enviar</button>
+        </form>
+      </div>
+    );
 }
       
  
